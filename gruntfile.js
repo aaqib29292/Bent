a32362ad915a3bc4a24c4ce9953beb58c4a73439
Aaqib Parvez <aaqib29292@gmail.com>
2016-10-31 23:08:56 +0530

    created files task for creating project folder structure

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,29 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-
-  });
+  // grunt.initConfig({
+  //
+  // });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('');
+
 
   // Default task(s).
-  grunt.registerTask('default', ['']);
+  grunt.registerTask("files", function() {
+      // making dir
+      grunt.file.mkdir('builds/dev/css');
+      grunt.file.mkdir('builds/dev/images');
+      grunt.file.mkdir('builds/dev/js');
+      grunt.file.mkdir('components/sass');
+      grunt.file.mkdir('components/js');
+
+      // writing files
+      grunt.file.write('builds/dev/index.html')
+      grunt.file.write('components/sass/style.scss');
+      grunt.file.write('components/js/script.js')
+
+  })
+
+  // grunt.registerTask('default', ['']);
 
 };

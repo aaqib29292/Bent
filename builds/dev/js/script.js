697bb9ce1a40d1cbcd74975c8b7ad6d0fb7caee3
aaqib29292 <aaqib29292@gmail.com>
2016-11-08 23:16:33 +0530

    completed screenshots section

--- builds/dev/js/script.js
+++ builds/dev/js/script.js
@@ -11,4 +11,14 @@ $( document ).ready(function() {
 
   $('.video').fitVids();
 
+  $("#owl-carousel").owlCarousel({
+
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+
+  });
+
 });
